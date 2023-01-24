89ee1fb6e7840fda63bc30ec28ce798926c9b815
Manuela <Miuuu132@gmail.com>
2023-01-24 14:48:30 +0100

    Project started...

--- Rover/src/main/scala/ownProject_Rover/Main.scala
+++ Rover/src/main/scala/ownProject_Rover/Main.scala
@@ -4,7 +4,7 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Hello I will be a project soon!")
 
-    val rover1 = new Rover("Charly", 1,2,3)
-    rover1.printStartingPosition()
+    val statRover1: StationaryRover = new StationaryRover("Charly", 1, 2, 3)
+    statRover1.printStartingPosition()
   }
 }
--- Rover/src/main/scala/ownProject_Rover/Rover.scala
+++ Rover/src/main/scala/ownProject_Rover/Rover.scala
@@ -1,9 +1,16 @@
 package ownProject_Rover
 
-class Rover(val name: String, val x: Int, val y: Int, z: Int) {
-
+abstract class Rover(val name: String, val x: Int, val y: Int, z: Int) {
+  def printName(): Unit = println(s"My name is $name. I am a Rover.")
   def printStartingPosition(): Unit = println(s"My starting position: $x, $y, $z")
 
 
 }
 
+
+class StationaryRover(name: String, x: Int, y: Int, z: Int)
+  extends Rover(name: String, x: Int, y: Int, z: Int){
+  override def printName(): Unit = super.printName() + "I am a stationary rover."
+  override def printStartingPosition(): Unit = println(s"My stationary position: $x, $y, $z - I can't move.")
+
+}
