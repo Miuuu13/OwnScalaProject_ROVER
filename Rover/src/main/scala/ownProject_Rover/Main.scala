6ccdfb9bcf151fcaf6888ddd0e1160c0f3550c47
Manuela <Miuuu132@gmail.com>
2023-01-24 14:40:01 +0100

    Project started...

--- Rover/src/main/scala/ownProject_Rover/Main.scala
+++ Rover/src/main/scala/ownProject_Rover/Main.scala
@@ -1,5 +1,10 @@
+package ownProject_Rover
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello I will be a project soon!")
+
+    val rover1 = new Rover("Charly", 1,2,3)
+    rover1.printStartingPosition()
   }
 }
---
+++ Rover/src/main/scala/ownProject_Rover/Rover.scala
@@ -0,0 +1,9 @@
+package ownProject_Rover
+
+class Rover(val name: String, val x: Int, val y: Int, z: Int) {
+
+  def printStartingPosition(): Unit = println(s"My starting position: $x, $y, $z")
+
+
+}
+
