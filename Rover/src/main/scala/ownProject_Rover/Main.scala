d371a58fcdbbead99a74938d922cdf43d30846c4
Manuela <Miuuu132@gmail.com>
2023-02-07 16:47:57 +0100

    Project started...

--- Rover/src/main/scala/ownProject_Rover/Main.scala
+++ Rover/src/main/scala/ownProject_Rover/Main.scala
@@ -5,6 +5,9 @@ object Main {
     println("Hello I will be a project soon!")
 
     val statRover1: StationaryRover = new StationaryRover("Charly", 1, 2, 3)
-    statRover1.printStartingPosition()
+    statRover1.printPosition()
+
+    val movRover1: MovingRover = new MovingRover("Runny", 0, 0, 0)
+    println(movRover1.move(1,2,3))
   }
 }
